723d59ac511f65eb13ca603a341dc69dadaff6df
ShootingersNew <lebedevseergey@gmail.com>
2024-12-24 06:59:25 +0300

    feat: add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/components/index.ts",
+      name: "meduza-pet-ui-kit",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': 'src',
+    },
+  },
 });
+

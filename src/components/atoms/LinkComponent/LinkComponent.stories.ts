568f2c53762fe95929577378e12e767bbfde4225
Sergey Lebedev <54789143+ShootingersNew@users.noreply.github.com>
2025-01-22 10:29:17 +0300

    Added LinkComponent (#5)
    * feat: export icon types
    
    * Added link component (#4)
    
    * fix(styles): variables
    
    * feat: add link component
    
    * feat: update package ver

---
+++ src/components/atoms/LinkComponent/LinkComponent.stories.ts
@@ -0,0 +1,20 @@
+import LinkComponent from './index.vue';
+import { ELinkTheme } from './models';
+export default {
+  title: 'Atoms/LinkComponent',
+  component: LinkComponent,
+  argTypes: {
+    theme : { control: 'select', options: Object.values(ELinkTheme) },
+    disabled: { control: 'boolean' },
+  },
+};
+
+const DefaultTemplate = (args) => ({
+  components: { LinkComponent },
+  setup() {
+    return { args };
+  },
+  template: `<LinkComponent  v-bind="args" href="https://youtu.be/dQw4w9WgXcQ">Click it</LinkComponent>`,
+});
+
+export const Default = DefaultTemplate.bind({});
